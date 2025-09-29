75f6e1520000f18f9a5b221fa6a03e66ba0ce7fc
SeongjeLee22200525 <seongjei646@gmail.com>
2025-09-29 10:54:24 +0900

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HI
+          <h1>내 이름은 이성제</h1>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
